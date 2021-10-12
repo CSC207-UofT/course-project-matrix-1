6b1b1f23ffd58fb16b02d4ad0d5ba6148fc5a0da
Sean Jeong <seandjeong@gmail.com>
2021-10-12 19:38:28 -0400

    made BE a little better

--- Phase0/src/BedmasEquation.java
+++ Phase0/src/BedmasEquation.java
@@ -2,10 +2,16 @@ public class BedmasEquation implements Equation{
     private String question;
     private String answer;
     public BedmasEquation(int firstNum, int secondNum, String operator){
-        this.question = (String) firstNum + operator + (strin)
+        this.question = (String) firstNum + operator + (String) secondNum
     }
     @Override
-    public void solve();
+    public void solve(){
+
+    }
+    @Override
+    public String[] getEquation(Equation eq){
+
+    }
 
 
 
