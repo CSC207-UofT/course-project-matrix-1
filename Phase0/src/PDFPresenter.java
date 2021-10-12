88caad3064ff87f22485ed0328068c342d535ec4
WilldJeong <willdjeong@gmail.com>
2021-10-12 16:28:36 -0400

    Completed PDFPresenter.java and PDFPresenterTest.java

--- Phase0/src/PDFPresenter.java
+++ Phase0/src/PDFPresenter.java
@@ -6,7 +6,7 @@
  * @since 2021-10-12
  */
 public class PDFPresenter {
-    public Worksheet ws;
+    public Worksheet ws; //TODO: Change to private once you can use WorksheetGenerator
 
     /**
      * Creates an instance of PDFPresenter that shares a worksheet with WorksheetGenerator.
@@ -21,23 +21,24 @@ public class PDFPresenter {
      * Uses the worksheet information stored in ws to create a new formatted PDF version of the worksheet, and stores
      * that inside ws.
      *
+     * @param title          the title of the PDF, displayed at the top
      * @param fontSize       the font size of the PDF
      * @param equationFormat how the equation should be displayed. For example, "horizontal" refers to 4 + 5, whereas
      *                       vertical refers to 4 + 5 stacked on top of each other
      */
-    public void createWorksheetPDF(int fontSize, String equationFormat) {
+    public void createWorksheetPDF(String title, int fontSize, String equationFormat) {
         String[][] equations = ws.getEquations();
         StringBuilder pdfNoAnswers = new StringBuilder();
         StringBuilder pdfWithAnswers = new StringBuilder();
-        pdfNoAnswers.append("Font size: ").append(fontSize).append(". Equation format: ")
-                .append(equationFormat).append(".").append("\n");
-        pdfWithAnswers.append("Font size: ").append(fontSize).append(". Equation format: ")
-                .append(equationFormat).append(".").append("\n");
+        pdfNoAnswers.append("Title: ").append(title).append(". Font size: ").append(fontSize).
+                append(". Equation format: ").append(equationFormat).append(".").append("\n");
+        pdfWithAnswers.append("Title: ").append(title).append(". Font size: ").append(fontSize).
+                append(". Equation format: ").append(equationFormat).append(".").append("\n");
         for (String[] equation : equations) {
             pdfNoAnswers.append(equation[0]).append(" = ___").append("\n");
             pdfWithAnswers.append(equation[0]).append(" = ").append(equation[1]).append("\n");
         }
-        String[] pdfs = {pdfNoAnswers.toString(), pdfWithAnswers.toString()};
+        String[] pdfs = {pdfNoAnswers.toString().strip(), pdfWithAnswers.toString().strip()};
         ws.setPDFs(pdfs);
     }
 
@@ -56,6 +57,10 @@ public class PDFPresenter {
      * @param path the file path specifying where the file should be downloaded
      */
     public void downloadPDF(String path) {
+        System.out.println("You downloaded these PDF's to " + path);
+        System.out.println(ws.getPDFs()[0]);
+        System.out.println(ws.getPDFs()[1]);
+
 
     }
 
--- Phase0/test/PDFPresenterTest.java
+++ Phase0/test/PDFPresenterTest.java
@@ -1,41 +1,29 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.Assert.*;
 
 public class PDFPresenterTest {
+
     private PDFPresenter p;
 
-
-    @BeforeEach
+    @org.junit.Before
     public void init() {
         Worksheet ws = new Worksheet();
-        PDFPresenter p = new PDFPresenter(ws);
+        p = new PDFPresenter(ws);
+        String[][] equations = {{"q1", "a1"}, {"q2", "a2"}};
+        p.ws.setEquations(equations); //remove later
+        p.createWorksheetPDF("Sample Worksheet",12, "Horizontal");
     }
 
-
-    @Test
+    @org.junit.Test
     public void testCreateWorksheetPDF() {
-        String [][] equations = {{"q1", "a1"},{"q2", "a2"}};
-        p.ws.setEquations(equations); //remove later
-        p.createWorksheetPDF(12, "Horizontal");
         String pdfNoAnswers = p.ws.getPDFs()[0];
         String pdfWithAnswers = p.ws.getPDFs()[1];
-        assertEquals(pdfNoAnswers, """
-                Font size: 12 . Equation format: Horizontal.
-                q1 = ___
-                q2 = ___
-                """);
-
-
+        assertEquals("Title: Sample Worksheet. Font size: 12. Equation format: Horizontal." + "\n" + "q1 = ___" + "\n" + "q2 = ___" , pdfNoAnswers);
     }
 
-    @Test
-    public void testGetPDFs() {
-    }
-
-    @Test
+    @org.junit.Test
     public void testDownloadPDF() {
+        p.downloadPDF("path/path/path");
     }
+
+
 }
