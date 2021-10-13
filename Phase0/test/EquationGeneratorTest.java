0aa6bca6f3812d2978d2f996d17f52c233739627
WilldJeong <willdjeong@gmail.com>
2021-10-12 20:31:27 -0400

    Finished skeleton program functionality

--- Phase0/test/EquationGeneratorTest.java
+++ Phase0/test/EquationGeneratorTest.java
@@ -4,9 +4,10 @@ import static org.junit.Assert.*;
 
 public class EquationGeneratorTest {
     EquationGenerator eg = new EquationGenerator();
+
     @Test
     public void testCreateValidEquation() {
-        String [] equation = eg.createEquation("standard add", 3);
+        String[] equation = eg.createEquation("standard add", 3);
         System.out.println(equation[0] + " = " + equation[1]);
         assertTrue(equation[0].length() < 10);
     }
@@ -14,9 +15,7 @@ public class EquationGeneratorTest {
     @Test
     public void testCreateInvalidEquation() {
         assertThrows(InvalidInputException.class,
-                () -> {
-                    eg.createEquation("standard add", 10);
-                }
-                );
+                () -> eg.createEquation("standard add", 10)
+        );
     }
 }
--- Phase0/test/PDFPresenterTest.java
+++ Phase0/test/PDFPresenterTest.java
@@ -3,21 +3,23 @@ import static org.junit.Assert.*;
 public class PDFPresenterTest {
 
     private PDFPresenter p;
+    private WorksheetGenerator wg;
 
     @org.junit.Before
     public void init() {
         Worksheet ws = new Worksheet();
+        wg = new WorksheetGenerator(ws);
         p = new PDFPresenter(ws);
-        String[][] equations = {{"q1", "a1"}, {"q2", "a2"}};
-        p.ws.setEquations(equations); //remove later
-        p.createWorksheetPDF("Sample Worksheet",12, "Horizontal");
+        wg.generateWorksheet("standard add", 10, 3);
+        //String[][] equations = {{"q1", "a1"}, {"q2", "a2"}};
+        p.createWorksheetPDF("Sample Worksheet", 12, "Horizontal");
     }
 
     @org.junit.Test
     public void testCreateWorksheetPDF() {
         String pdfNoAnswers = p.ws.getPDFs()[0];
         String pdfWithAnswers = p.ws.getPDFs()[1];
-        assertEquals("Title: Sample Worksheet. Font size: 12. Equation format: Horizontal." + "\n" + "q1 = ___" + "\n" + "q2 = ___" , pdfNoAnswers);
+        assertEquals("Title: Sample Worksheet. Font size: 12. Equation format: Horizontal.", pdfNoAnswers.substring(0,68));
     }
 
     @org.junit.Test
