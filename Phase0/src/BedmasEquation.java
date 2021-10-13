6f0412ac7a3993c0d9fbc992f4cefc703b6cc955
Sean Jeong <seandjeong@gmail.com>
2021-10-12 20:32:10 -0400

    Added javadoc comments

--- Phase0/src/BedmasEquation.java
+++ Phase0/src/BedmasEquation.java
@@ -1,3 +1,10 @@
+/**
+ * A Bedmas equation interface class. This includes any BEDMAS types of problems, and will allow for fractions as well as integers (for now just integers).
+ *
+ * @author Sean Jeong
+ * @version 1.0
+ * @since 2021-10-12
+ */
 public class BedmasEquation implements Equation{
     private final String question;
     private String answer;
@@ -5,11 +12,19 @@ public class BedmasEquation implements Equation{
     public BedmasEquation(int firstNum, int secondNum, String operator) {
         this.question = firstNum + " " + operator + " " + secondNum;
     }
+    /**
+     * Solve the equation. It takes the question String and calculates an answer String and saves it under answer.
+     */
     @Override
     public void solve(){
         this.answer = "answer";
     }
 
+    //TODO: finish comments
+    /**
+     * Create a List
+     * @return a List of String representation of the question and answer in this BedmasEquation.
+     */
     @Override
     public String[] getEquation(){
         return new String[]{this.question, this.answer};
--- Phase0/src/Equation.java
+++ Phase0/src/Equation.java
@@ -1,3 +1,10 @@
+/**
+ * An Equation interface class.
+ *
+ * @author Sean Jeong
+ * @version 1.0
+ * @since 2021-10-12
+ */
 public interface Equation {
     void solve();
     String[] getEquation();
