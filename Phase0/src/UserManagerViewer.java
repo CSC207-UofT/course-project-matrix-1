14d6abbf0dba816c1698ab5f1b836fa1b64bccfd
kerimsalt <kerim.saltoglu@mail.utoronto.ca>
2021-10-14 18:47:12 -0400

    Almost done with UserManagerViewer

--- Phase0/src/UserManagerViewer.java
+++ Phase0/src/UserManagerViewer.java
@@ -1,39 +1,46 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class UserManagerViewer {
     // txt file to store all the users?
     // load the users that are stored in the txt
     // UserManager Class creates an arraylist of user objects. It passes that list to both viewer and Manager
 
-    // TODO: Finish verifyUsername!
-    // TODO: Test verifyUsername!
-    private ArrayList<User> users;
-    public UserManagerViewer(ArrayList<User> users){this.users = users;}
+
+    private List<User> users;  // This variable is aliased with UserManagerUpdater.users variable
+    public UserManagerViewer(List<User> users){this.users = users;}
     public boolean verifyUsername(String username){
-        //TODO: Actually implement it HERE!
-        // the arraylist of users between viewer updater
-        return true; // I just created this method so that my UserController would work
+        for(User u: this.getUsers()){
+            if(u.getUsername().equals(username)){return true;}
+        }
+        return false;
     }
 
-    public ArrayList<User> getUsers() {
+    public List<User> getUsers() {
         return users;
     }
 
-    public HashMap<String, Object> getUserDetails(){
-        return new HashMap<>();  // I just created this method so that my UserController would work
-        //getUserScores
+    public HashMap<String, Object> getUserDetails(String username){
+        for(User u: this.users){
+            if(u.getUsername().equals(username)){return u.getDetails();}
+        }
+        return new HashMap<>();  //Return empty hash map if the username is invalid
     }
 
-    public HashMap<String, Integer> getUserScores(){
-        return new HashMap<>();  // I just created this method so that my UserController would work
+    public HashMap<String, Integer> getUserScores(String username){
+        for(User u: this.users){
+            if(u.getUsername().equals(username)){return u.getScores();}
+        }
+        return new HashMap<>();  //Return empty hash map if the username is invalid
     }
 
-    public ArrayList<HashMap<String, Object>> getUserHistory(){
-        // key: String Date of the action of the user
-        // value: Description of the action of the user
+    public ArrayList<HashMap> getUserHistory(String username){
+        for(User u: this.users){
+            if(u.getUsername().equals(username)){return u.getHistory();}
+        }
         // Note: The return type was not specified in the CRC cards so feel free to change it
-        return new ArrayList<HashMap<String, Object>>();  // I just created this method so that my UserController would work
+        return new ArrayList<>(); //Return empty arraylist if the username is invalid
     }
 
 }
