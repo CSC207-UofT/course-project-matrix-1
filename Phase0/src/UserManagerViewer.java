d3c2686561e448399c5e7003c210bf82e3e675f6
kerimsalt <kerim.saltoglu@mail.utoronto.ca>
2021-10-13 21:38:57 -0400

    Return types updated.

--- Phase0/src/UserManagerViewer.java
+++ Phase0/src/UserManagerViewer.java
@@ -1,28 +1,32 @@
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 public class UserManagerViewer {
-    public UserManagerViewer(){
+    // txt file to store all the users?
+    // load the users that are stored in the txt
+    // UserManager Class creates an arraylist of user objects. It passes that list to both viewer and Manager
 
-    }
+    public UserManagerViewer(){}
+
     public boolean verifyUsername(String username){
+        // Actually implement
         return true; // I just created this method so that my UserController would work
     }
 
-    public HashMap<String, String> getUserDetails(){
+    public HashMap<String, Object> getUserDetails(){
         return new HashMap<>();  // I just created this method so that my UserController would work
         //getUserScores
     }
 
-    public HashMap<String, String> getUserScores(){
+    public HashMap<String, Integer> getUserScores(){
         return new HashMap<>();  // I just created this method so that my UserController would work
     }
 
-    public HashMap<String, String> getUserHistory(){
+    public ArrayList<HashMap<String, Object>> getUserHistory(){
         // key: String Date of the action of the user
         // value: Description of the action of the user
         // Note: The return type was not specified in the CRC cards so feel free to change it
-        return new HashMap<>();  // I just created this method so that my UserController would work
+        return new ArrayList<HashMap<String, Object>>();  // I just created this method so that my UserController would work
     }
 
 }
