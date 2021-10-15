db2c6596d7b12ef52dfe9b91f8dd36d2ea427865
kerimsalt <kerim.saltoglu@mail.utoronto.ca>
2021-10-15 11:14:43 -0400

    Finished the tests for UserManager.

--- Phase0/test/UserManagerTest.java
+++ Phase0/test/UserManagerTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
 public class UserManagerTest {
+    /**
+     * Create instance of User class to test methods.
+     */
+    UserManager newManager;
+
+
+    @Test
+    public void testCreateViewerAndUpdater(){
+        List<Object> managers = UserManager.createViewerAndUpdater();
+        UserManagerViewer viewer = (UserManagerViewer) managers.get(0);
+        UserManagerUpdater updater = (UserManagerUpdater) managers.get(1);
+        assert viewer.verifyUsername("main");
+        assert updater.getUsers().get(0).getUsername().equals("main");
+    }
 }
