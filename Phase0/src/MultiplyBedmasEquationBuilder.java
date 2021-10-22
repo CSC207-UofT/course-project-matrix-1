4548e8c3d09099f89335d06988c8942c15a8571c
Sean Jeong <seandjeong@gmail.com>
2021-10-21 20:06:25 -0400

    Created BedmasEquationBuilder classes

---
+++ Phase0/src/MultiplyBedmasEquationBuilder.java
@@ -0,0 +1,14 @@
+public class MultiplyBedmasEquationBuilder extends BedmasEquationBuilder{
+    public void buildOperator(){
+
+    }
+    public void buildAnswer(){
+
+    }
+    public void buildOperand1(){
+
+    }
+    public void buildOperand2(){
+
+    }
+}
---
+++ Phase0/src/SubBedmasEquationBuilder.java
@@ -0,0 +1,14 @@
+public class SubBedmasEquationBuilder extends BedmasEquationBuilder{
+    public void buildOperator(){
+
+    }
+    public void buildAnswer(){
+
+    }
+    public void buildOperand1(){
+
+    }
+    public void buildOperand2(){
+
+    }
+}
