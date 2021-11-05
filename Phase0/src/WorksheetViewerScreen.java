da927b411896e5ad37deb98c082583e7bd6d95c4
Ethan <ethan.ing@mail.utoronto.ca>
2021-11-05 12:22:29 -0400

    updated UserInterface with GUI

--- Phase0/src/WorksheetViewerScreen.java
+++ Phase0/src/WorksheetViewerScreen.java
@@ -1,19 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
-public class WorksheetViewerScreen extends StartScreen{
+import java.awt.event.MouseListener;
+
+public class WorksheetViewerScreen extends StartScreen implements MouseListener {
+
+    JPanel panel4 = new JPanel();
+
     public WorksheetViewerScreen(){
         // Set Frame
-        JFrame frame3 = new JFrame();
-        frame3.setSize(width, height);
-        frame3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 
         // Set Panel and Border
         JPanel panel3 = new JPanel();
         panel3.setBorder(BorderFactory.createMatteBorder(1, convert(0.1, 'w'), 1,
                 convert(0.1, 'h'), Color.BLACK));
         panel3.setLayout(null);
-        frame3.add(panel3);
-        frame3.setVisible(true);
+        frame.add(panel3);
+        frame.setVisible(true);
         frame.setVisible(false);
     }
 }
