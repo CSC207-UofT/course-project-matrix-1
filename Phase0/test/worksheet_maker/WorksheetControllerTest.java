165ead4cfa30991792195c8ff20dfb14a398c6b8
WilldJeong <willdjeong@gmail.com>
2021-11-07 11:15:17 -0500

    Increased PDF resolution

--- Phase0/test/worksheet_maker/WorksheetControllerTest.java
+++ Phase0/test/worksheet_maker/WorksheetControllerTest.java
@@ -6,15 +6,16 @@ import org.junit.Test;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class WorksheetControllerTest {
     @Test
     public void WorksheetControllerVisualTest() throws IOException {
         HashMap<String, Object> myEquationDetails = new HashMap<>();
-        myEquationDetails.put("numOfEquations", 50);
-        myEquationDetails.put("operator", '*');
-        myEquationDetails.put("operandRange1", new int[]{1, 9});
-        myEquationDetails.put("operandRange2", new int[]{1, 9});
+        myEquationDetails.put("numOfEquations", 100);
+        myEquationDetails.put("operator", '/');
+        myEquationDetails.put("operandRange1", new int[]{1, 100});
+        myEquationDetails.put("operandRange2", new int[]{5, 9});
         myEquationDetails.put("negAllowed", true);
         HashMap<String, Object> myFormatDetails = new HashMap<>();
         myFormatDetails.put("equationFormat", "Horizontal");
@@ -23,10 +24,21 @@ public class WorksheetControllerTest {
         myFormatDetails.put("numColumns", 3);
         WorksheetController wc = new WorksheetController();
         PDDocument[] pdf = wc.generateWorksheetAndPDF(myEquationDetails, myFormatDetails);
-        pdf[0].save("C:/Users/willj/Downloads/questions.pdf");
+        String path = "C:/Users/willj/Downloads/";
+        pdf[0].save(path + "/questions.pdf");
         pdf[0].close();
-        pdf[1].save("C:/Users/willj/Downloads/answers.pdf");
+        pdf[1].save(path + "answers.pdf");
         pdf[1].close();
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter a path to save the questions and answers: ");
+//        String path = sc.nextLine();
+//        if (path.equals("")) {
+//            path = "C:/Users/willj/Downloads/";
+//        }
+//        pdf[0].save(path + "/questions.pdf");
+//        pdf[0].close();
+//        pdf[1].save(path + "answers.pdf");
+//        pdf[1].close();
     }
 
 
