f389fcc3ac4e4ff16d0d83f168999590613cff88
WilldJeong <willdjeong@gmail.com>
2021-11-16 21:21:10 -0500

    Added basic fraction addition, but there are some bugs in randomization

---
+++ main/test/equation_builders/FractionDirectorTest.java
@@ -0,0 +1,35 @@
+package equation_builders;
+
+import equation_entities.Symbol;
+import equation_entities.WholeNum;
+import equation_parameters.FractionEquationDetails;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+public class FractionDirectorTest {
+    private FractionDirector director;
+    private int randomSeed;
+    private FractionEquationDetails fractionEquationDetails = new FractionEquationDetails(generateRange(1,10), 20, 4);
+
+    @Before
+    public void init() {
+        director = new FractionDirector();
+        randomSeed = new Random().nextInt(100000);
+        fractionEquationDetails.setOperator('+');
+        fractionEquationDetails.setNegAllowed(false);
+    }
+
+    @Test
+    public void testAddPos() {
+        director.setEquationBuilder('+');
+        director.constructEquation(fractionEquationDetails, randomSeed + 5);
+        System.out.println(director.getEquation().getEquation());
+    }
+    public int[] generateRange(int min, int max) {
+        return new int[]{min, max};
+    }
+}
