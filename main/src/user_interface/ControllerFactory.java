090d2f872b7c0337e9bee1255c7cf18332a957a3
kerimsalt <kerim.saltoglu@mail.utoronto.ca>
2021-11-22 13:12:56 -0500

    TODO: Change the name of the ControllerFactory.

--- main/src/user_interface/ControllerFactory.java
+++ main/src/user_interface/ControllerFactory.java
@@ -4,6 +4,7 @@ import user_package.UserController;
 import worksheet_maker.WorksheetController;
 
 public class ControllerFactory {
+    //TODO: Refactor the of this class
     UserController makeUserController() {
         // Create an instance of user controller to keep track of the user's information
         UserController userController = null;
